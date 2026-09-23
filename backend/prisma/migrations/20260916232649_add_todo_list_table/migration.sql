b0a9dbe388cd5c54ccec8aa2caf863ddfa2c3a75
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-09-23 21:44:29 +0200

    fix(db): restore applied todo list migration checksum

--- backend/prisma/migrations/20260916232649_add_todo_list_table/migration.sql
+++ backend/prisma/migrations/20260916232649_add_todo_list_table/migration.sql
@@ -1,3 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `Todo` table. All the data in the column will be lost.
+  - Added the required column `listId` to the `Todo` table without a default value. This is not possible if the table is not empty.
+
+*/
 -- DropForeignKey
 ALTER TABLE "Todo" DROP CONSTRAINT "Todo_userId_fkey";
 
